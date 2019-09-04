d04064724533fc8fdcbee10040a435ecbbaa32d8
YangLi <345462072@qq.com>
2019-09-05 01:39:54 +0800

    20190905

---
+++ 201909/201909/20190905 组队竞赛 贪心 排序.cpp
@@ -0,0 +1,30 @@
+// Topic link：https://www.nowcoder.com/questionTerminal/6736cc3ffd1444a4a0057dee89be789b?orderByHotValue=1&page=1&onlyReference=false
+// 同步CSDN
+
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	// 注意在此会有多组测试用例，需要持续接收
+	int n;
+	while (cin >> n) {
+		long long sum = 0;
+		vector<int> a;
+		a.resize(3 * n);
+		for (int i = 0; i < (3 * n); i++) {
+			cin >> a[i];
+		}
+		// 排序，然后取a向量下标从n开始，后续步长为2，相加即可
+		std::sort(a.begin(), a.end());
+		for (int i = n; i <= 3 * n - 2; i += 2) {
+			sum += a[i];
+		}
+		cout << sum << endl;
+	}
+}
+

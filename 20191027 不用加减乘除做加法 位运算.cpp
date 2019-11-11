3b682b130312929cc45ae635d057b1fc422cb9b7
YangLi <345462072@qq.com>
2019-11-11 22:36:20 +0800

    20191027

---
+++ 20191027 不用加减乘除做加法 位运算.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+	int Add(int num1, int num2)
+	{
+		while (num2 != 0) {
+			int sum = num1 ^ num2;
+			int carray = (num1 & num2) << 1;
+			num1 = sum;
+			num2 = carray;
+		}
+		return num1;
+	}
+};
